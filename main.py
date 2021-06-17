94cad87c1a0f81711b11dd45036d4cb9f5f5d91a
pfftkhaganate <pfft.khaganate@gmail.com>
2021-06-17 19:43:12 +0100

    Basic trading bot *from https://www.youtube.com/watch?v=SEQbb8w7VTw

--- main.py
+++ main.py
@@ -14,11 +14,70 @@ AAPL = pd.read_csv('AAPL.csv')
 #Show the data
 #print(AAPL)
 
-#Visualize tha data
-plt.figure(figsize=(12.5,4.5))
-plt.plot(AAPL['Adj Close'], label='AAPL')
+
+
+#Create the simple moving average with a 30 day window
+SMA30 = pd.DataFrame()
+SMA30['Adj Close'] = AAPL['Adj Close'].rolling(window=30).mean()
+print(SMA30)
+
+#Create the simple moving average with a 100 day window
+SMA100 = pd.DataFrame()
+SMA100['Adj Close'] = AAPL['Adj Close'].rolling(window=100).mean()
+print(SMA100)
+
+
+#Create a new data frame to store all the data
+data = pd.DataFrame()
+data['AAPL'] = AAPL['Adj Close']
+data['SMA30'] = SMA30['Adj Close']
+data['SMA100'] = SMA100['Adj Close']
+
+
+#Create a function to signal when to buy and sell the asset/stock
+def buy_sell(data):
+    sigPriceBuy = []
+    sigPriceSell = []
+    flag = -1
+
+    for i in range(len(data)):
+        if data['SMA30'][i] > data['SMA100'][i]:
+            if flag != 1:
+                sigPriceBuy.append(data['AAPL'][i])
+                sigPriceSell.append(np.nan)
+                flag = 1
+            else:
+                sigPriceBuy.append(np.nan)
+                sigPriceSell.append(np.nan)
+        elif data['SMA30'][i] < data['SMA100'][i]:
+            if flag != 0:
+                sigPriceBuy.append(np.nan)
+                sigPriceSell.append(data['AAPL'][i])
+                flag = 0
+            else:
+                sigPriceBuy.append(np.nan)
+                sigPriceSell.append(np.nan)
+        else:
+            sigPriceBuy.append(np.nan)
+            sigPriceSell.append(np.nan)
+    return (sigPriceBuy, sigPriceSell)
+#Store the buy and sell data into a variable
+buy_sell = buy_sell(data)
+print(buy_sell)
+data['Buy_Signal_Price'] = buy_sell[0]
+data['Sell_Signal_Price'] = buy_sell[1]
+#Visualize tha data and strategy to buy and sell stock
+plt.figure(figsize=(12.6, 4.6))
+plt.plot(AAPL['Adj Close'], label='AAPL', alpha = 0.35)
+plt.plot(SMA30['Adj Close'], label='SMA30', alpha = 0.35)
+plt.plot(SMA100['Adj Close'], label='SMA100', alpha = 0.35)
+plt.scatter(data.index, data['Buy_Signal_Price'], label='Buy', marker='^', color='green')
+plt.scatter(data.index, data['Sell_Signal_Price'], label='Sell', marker='v', color='red')
 plt.title('Apple Adj. Close Price History')
 plt.xlabel('17/6/2021 - 17/6/2021')
 plt.ylabel('Adj. Close Price USD ($)')
 plt.legend(loc='upper left')
 plt.show()
+
+print(data)
+
